a76e9ae14bed5072c5a1b0676c8ce821818e6078
Khachatur <khachatur.hakobyan1993gmail.com>
2019-04-24 20:55:15 +0400

    Initial Project Directory

---
+++ Xam Weather/Xam WeatherTests/Xam_WeatherTests.swift
@@ -0,0 +1,34 @@
+//
+//  Xam_WeatherTests.swift
+//  Xam WeatherTests
+//
+//  Created by Khachatur Hakobyan on 4/24/19.
+//  Copyright © 2019 Khachatur Hakobyan. All rights reserved.
+//
+
+import XCTest
+@testable import Xam_Weather
+
+class Xam_WeatherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
